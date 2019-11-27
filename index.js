1556f8d48dd98c6024923f371543d0781e192c91
Philip-Owen <31570473+Philip-Owen@users.noreply.github.com>
2019-11-27 13:34:27 -0600

    updated runtime to node8.10 and refactored for use with new portfolio site

--- index.js
+++ index.js
@@ -7,46 +7,43 @@ const app = express();
 require('dotenv').config();
 
 const corsOptions = {
-  origin: function(origin, callback) {
-    if(origin == process.env.URL) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  }
+	origin: function(origin, callback) {
+		if (origin == process.env.URL) {
+			callback(null, true);
+		} else {
+			callback(new Error('Not allowed by CORS'));
+		}
+	},
 };
 
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.post('/', cors(corsOptions),(req,res,next) => {
+app.post('/', cors(corsOptions), (req, res, next) => {
+	const { name, email, message } = req.body;
 
-  let name = req.body.name;
-  let email = req.body.email;
-  let message = req.body.message;
+	const transporter = nodemailer.createTransport({
+		service: 'gmail',
+		auth: {
+			user: process.env.SENDEREMAIL,
+			pass: process.env.SENDERPASS,
+		},
+	});
 
-  var transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-      user: process.env.SENDEREMAIL,
-      pass: process.env.SENDERPASS
-    }
-  });
+	const mailOptions = {
+		from: process.env.SENDEREMAIL,
+		to: process.env.DESTEMAIL,
+		subject: 'From Portfolio Contact Form',
+		text: `From: ${name} @ ${email}, Message: ${message}`,
+	};
 
-  var mailOptions = {
-    from: process.env.SENDEREMAIL,
-    to: process.env.DESTEMAIL,
-    subject: "From Portfolio Contact Form",
-    text: `From: ${name} @ ${email}, Message: ${message}`,
-  };
-
-  transporter.sendMail(mailOptions, function(error, info){
-    if (error) {
-      res.sendStatus(500);
-    } else {
-      res.sendStatus(200);
-    }
-  });
-
+	transporter.sendMail(mailOptions, function(error, info) {
+		if (error) {
+			res.sendStatus(500);
+		} else {
+			res.sendStatus(200);
+		}
+	});
 });
 
 module.exports.handler = serverless(app);
